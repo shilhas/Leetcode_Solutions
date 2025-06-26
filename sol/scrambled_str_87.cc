451ca5fa40385fc030658541e873d446e8907272
Shihas Radhakrishnan <shilhas@gmail.com>
2025-06-26 15:12:33 +0200

    Optimized solution for LC 87 and 131

--- sol/scrambled_str_87.cc
+++ sol/scrambled_str_87.cc
@@ -6,45 +6,36 @@
 using namespace std;
 
 class Solution {
-    unordered_map<string, set<string>>dp;
+    unordered_map<string, bool>dp;
 public:
-    set<string> get_scrambled(string s1) {
-        set<string> ret;
-        if (dp.find(s1) != dp.end()) return dp[s1];
-        if (s1.length() == 1) {
-            ret.insert(s1);
-            dp[s1] = ret;
-            return ret;
-        }
-        
-        for(int i = 1; i < s1.length(); i++) {
-            // string tmp1 = s1.substr(0,i);
-            // string tmp2 = s1.substr(i);
-            auto sc1 = get_scrambled(s1.substr(0,i));
-            auto sc2 = get_scrambled(s1.substr(i));
-            for (auto p1: sc1) {
-                for (auto p2: sc2) {
-                    ret.insert(p1+p2);
-                    ret.insert(p2+p1);
-                }
-            }
-        }
-        dp[s1] = std::move(ret);
-        return dp[s1];
-    }
     
     bool isScramble(string s1, string s2) {
         // divide string s1 from index 1 to n - 1
         // check create swapped and unswapped version
         // if len of any part > 1 repeat step
+        int len = s1.length();
         if (s1 == s2) return true;
+        if (len == 1) return false;
+
+        if (dp.find(s1+s2) != dp.end()) return dp[s1+s2];
         
-        set<string> scrambled = get_scrambled(s1);
-        for (auto v: scrambled) {
-            if (v == s2) return true;
+        auto split_str = [](string& str, int i) -> pair<string, string>{
+            return make_pair(str.substr(0,i), str.substr(i));
+        };
+        auto key = s1 + s2;
+        for (int i = 1; i < len; i++) {
+            auto s1_split = split_str(s1, i);
+            auto s2_split = split_str(s2, i);
+            if (isScramble(s1_split.first, s2_split.first) && isScramble(s1_split.second, s2_split.second)){
+                return dp[key] = true;
+            }
+            s2_split = split_str(s2, len-i);
+            if (isScramble(s1_split.first, s2_split.second) && isScramble(s1_split.second, s2_split.first)){
+                return dp[key] = true;
+            }
         }
         
-        return false;
+        return dp[s1+s2] = false;
     }
 };
 
@@ -57,16 +48,16 @@ int main () {
     };
     for (auto test: tests) {
         Solution sol;
-        // auto measure_delta = [](auto start){
-        //     auto end = chrono::steady_clock::now();
-        //     cout << "\n\nTime needed for computation: " << chrono::duration_cast<chrono::microseconds>(end - start).count() << "\n\n";
-        // };
-        // auto start = chrono::steady_clock::now();
+        auto measure_delta = [](auto start){
+            auto end = chrono::steady_clock::now();
+            cout << "\nTime needed for computation: " << chrono::duration_cast<chrono::microseconds>(end - start).count() << "\n\n";
+        };
+        auto start = chrono::steady_clock::now();
         if (sol.isScramble(test[0], test[1])) {
             cout << test[1] << " is scrambled version of " << test[0]<<endl;
         } else {
             cout << test[1] << " is not scrambled version of " << test[0]<<endl;
         }
-        // measure_delta(start);
+        measure_delta(start);
     }
 }
--- sol/sub_palindrome_131.cc
+++ sol/sub_palindrome_131.cc
@@ -7,25 +7,16 @@ using namespace std;
 
 class Solution {
     vector<vector<string>> res;
+    vector<vector<int>> dp;
 public:
-    bool isPalindrome(string& s) {
-        if (s.size() == 1) return true;
-        int l = 0;
-        int r = s.size() - 1;
-        while(l <= r) {
-          if (s[l] != s[r]) return false;
-          l++; r--;
-        }
-        return true;
-    }
     void dfs(int i, string& s, vector<string>& part) {
         if (i >= s.size()) {
             res.push_back(part);
             return;
         }
         for (int j = i; j < s.size(); j++) {
-            auto substr = s.substr(i, (j + 1 - i));
-            if (isPalindrome(substr)) {
+            auto substr = s.substr(i, (j+1 - i));
+            if (dp[i][j] == 1) {
                 part.push_back(substr);
                 dfs(j+1, s, part);
                 part.pop_back();
@@ -35,6 +26,22 @@ public:
     vector<vector<string>> partition(string s) {
         if (s.size() < 2) return {{s}};
         vector<string> part;
+        int slen = s.size();
+        dp.resize(slen, vector<int>(slen, -1));
+        for (int i = 0; i < slen; i++) {
+            dp[i][i] = 1;
+        }
+        auto check_dp = [](int dp_val) {return dp_val == -1 or dp_val == 1;};
+        for (int i = slen - 2; i > -1; i--) {
+            for (int j = i+1; j < slen; j++) {
+                if (s[i] == s[j] and check_dp(dp[i+1][j-1])) {
+                    dp[i][j] = 1;
+                } else {
+                    dp[i][j] = 0;
+                }
+            }
+        }
+
         dfs(0, s, part);
         return this->res;
     }
@@ -42,7 +49,7 @@ public:
 
 
 int main () {
-    vector<string> tests = {"cdd","titfortat", "aab", "malayalam"};
+    vector<string> tests = {"coop", "canon", "aab", "malayalam"};
     for (auto test: tests) {
         Solution sol;
         cout << "Input: " << test << "\t";
