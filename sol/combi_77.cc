4798207cde738bb13849c5b49077f7ffe084d83d
Shihas Radhakrishnan <shilhas@gmail.com>
2025-08-14 17:18:17 +0530

    Optimized solution for LC 77

--- sol/combi_77.cc
+++ sol/combi_77.cc
@@ -7,37 +7,25 @@ using namespace std;
 
 class Solution {
 public:
-    vector<vector<int>> get_combi(int& n, int k, int start) {
-        if (start+k > n+1 or k == 0) return {{}};
-        
-        vector<vector<int>>ret;
-        if (k == 1) {
-            ret.resize(n+1-start, vector<int>(1));
-            int indx = 0;
-            for (int i = start; i <= n; i++) {
-                ret[indx][0] = i;
-                indx++;
-            }
-            return ret;
+    void get_combi(int& n, int k, int start, vector<int>&curr, vector<vector<int>>&res) {
+        if (curr.size() == k) {
+            res.push_back(curr);
+            return;
         }
-        for (int i = start; i < n; i++) {
-            // get k-1 combination from start + 1
-            auto tmp2 = get_combi(n, k-1, i+1);
-            for (int j = 0; j < tmp2.size(); j++) {
-                if (tmp2[j].size() != k-1) {
-                    tmp2.clear();
-                    break;
-                }
-                tmp2[j].push_back(i);
-            }
-            if (tmp2.size() > 0)
-            ret.insert(ret.end(), tmp2.begin(), tmp2.end());
+        if (start > n) return;
+
+        for(int i = start; i <= n; i++) {
+            curr.push_back(i);
+            get_combi(n, k, i+1,curr, res);
+            curr.pop_back();
         }
-        return ret;
     } 
 
     vector<vector<int>> combine(int n, int k) {
-        return get_combi(n, k, 1);
+        vector<vector<int>>res;
+        vector<int>curr;
+        get_combi(n, k, 1, curr, res);
+        return res;
     }
 };
 
